c189535f6f23724193ccddcbc7faad707bce3a73
AmalNasharti <132660624+AmalNasharti@users.noreply.github.com>
2026-08-20 18:32:04 +0200

    Optimized ESN with professor's slides

--- source_code/esn_model.py
+++ source_code/esn_model.py
@@ -87,17 +87,29 @@ class EchoStateNetwork(nn.Module):
         Desired spectral radius of the recurrent reservoir matrix.
     """
 
-    def __init__(self, reservoir_size, spectral_radius):
+    def __init__(self, reservoir_size, spectral_radius, reservoir_connectivity, input_scaling, alpha, washout, ridge):
         super().__init__()
 
         self.reservoir_size = reservoir_size
+        self.alpha = alpha
+        self.washout = washout
+        self.ridge = ridge
 
-        # Reservoir weights
-        W_res = torch.rand(
+        # Random reservoir weights in [-1, 1]
+        W_res = 2 * torch.rand(
             reservoir_size,
             reservoir_size,
             dtype=torch.float32
-        ) - 0.5
+        ) - 1
+
+        # Random connectivity mask
+        mask = torch.rand(
+            reservoir_size,
+            reservoir_size
+        ) <= reservoir_connectivity
+
+        # Sparsity requirement of reservoir weights
+        W_res = W_res * mask
 
         # Compute current spectral radius
         eigenvalues = torch.linalg.eigvals(W_res)
@@ -110,11 +122,13 @@ class EchoStateNetwork(nn.Module):
         self.register_buffer("W_res", W_res)
 
         # Input weights
-        W_in = torch.rand(
-            reservoir_size,
-            1,
-            dtype=torch.float32
-        ) - 0.5
+        W_in = (
+            2 * torch.rand(
+                reservoir_size,
+                1,
+                dtype=torch.float32
+            ) - 1
+        ) * input_scaling
 
         self.register_buffer("W_in", W_in)
 
@@ -142,11 +156,14 @@ class EchoStateNetwork(nn.Module):
             device=input_data.device
         )
 
+        # Leaky integration
         for t in range(1, len(input_data)):
-
-            reservoir_states[t] = torch.tanh(
-                self.W_res @ reservoir_states[t - 1]
-                + (self.W_in @ input_data[t]).squeeze()
+            reservoir_states[t] = (
+                self.alpha * reservoir_states[t - 1]
+                + (1 - self.alpha) * torch.tanh(
+                    self.W_res @ reservoir_states[t - 1]
+                    + (self.W_in @ input_data[t]).squeeze()
+                )
             )
 
         return reservoir_states
@@ -165,10 +182,21 @@ class EchoStateNetwork(nn.Module):
 
         reservoir_states = self.run_reservoir(input_data)
 
-        # Compute output weights analytically
-        self.W_out = (
-            torch.linalg.pinv(reservoir_states)
-            @ target_data
+        # Discard the washout phase
+        X = reservoir_states[self.washout:]
+        Y = target_data[self.washout:]
+
+        # Identity matrix for ridge regularization
+        I = torch.eye(
+            X.shape[1],
+            device=X.device,
+            dtype=X.dtype
+        )
+
+        # Compute output weights using ridge regression
+        self.W_out = torch.linalg.solve(
+            X.T @ X + self.ridge * I,
+            X.T @ Y
         )
 
     def forward(self, input_data):
@@ -232,10 +260,17 @@ def run_esn(train_norm, val_norm, test_norm, config, device):
     # Model initialization
     model = EchoStateNetwork(
         reservoir_size=config["reservoir_size"],
-        spectral_radius=config["spectral_radius"]
+        spectral_radius=config["spectral_radius"],
+        reservoir_connectivity=config["reservoir_connectivity"],
+        input_scaling=config["input_scaling"],
+        alpha = config["alpha"],
+        washout = config["washout"],
+        ridge=config["ridge"]
     ).to(device)
     X_train = X_train.to(device)
     y_train = y_train.to(device)
+    X_val = X_val.to(device)
+    X_test = X_test.to(device)
 
     # Training model
     if device.type == "cuda":
